147a178c81c8e1c40df810f36ac62d8015af08b5
Raj <raj350@mail.fresnostate.edu>
2022-07-29 23:13:14 -0700

    workingo n inputfield

---
+++ frontend/src/components/InputField.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <label
+      class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+      :for="for"
+    >
+      {{ label }}
+    </label>
+    <input
+      class="
+        appearance-none
+        block
+        w-full
+        bg-gray-200
+        text-gray-700
+        border border-gray-200
+        rounded
+        py-3
+        px-4
+        leading-tight
+        focus:outline-none focus:bg-white focus:border-gray-500
+      "
+      type={{type}}
+      placeholder="{{ placeholder }}"
+      name="{{ for }}"
+    />
+  </div>
+</template>
+
+
+<script>
+export default {
+    props: ["for","label","type", "placeholder" ],
+    emits: ["returnValue"]
+
+}
+</script>
+
+<style scoped>
+</style>
