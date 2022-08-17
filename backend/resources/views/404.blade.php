878586cca46c1f10e9668404a663fe1b68e14390
Raj Solanki <raj350@mail.fresnostate.edu>
2022-08-17 12:04:18 -0700

    testing shop controller

---
+++ backend/resources/views/404.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 |  {{ config("app.name") }}  </title>
+</head>
+<body>
+    <div class="404__section">
+        <p>404 Error</p>
+        <p>Return back to <a href="https://partners.shopify.com/">Shopify</a></p>
+    </div>
+</body>
+</html>
