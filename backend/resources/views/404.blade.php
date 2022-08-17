147a6ccfac62f88545a5799b68e3b95ae020a020
Raj Solanki <raj350@mail.fresnostate.edu>
2022-08-17 13:14:29 -0700

    added in 404 page

--- backend/resources/views/404.blade.php
+++ backend/resources/views/404.blade.php
@@ -5,11 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>404 |  {{ config("app.name") }}  </title>
+    <link href="{{ asset('css/404.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div class="404__section">
-        <p>404 Error</p>
-        <p>Return back to <a href="https://partners.shopify.com/">Shopify</a></p>
+    <div class="redirect__section">
+        <div class="redirect__section-left">
+            <p>404 Error</p>
+        </div>
+
+        <div class="redirect__section-right">
+            <p>Return back to <a href="https://partners.shopify.com/">Shopify</a></p>
+        </div>
+        
     </div>
 </body>
 </html>
