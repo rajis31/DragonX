beb428687e6104baaa9c3abebd5050f42c4cf2da
Raj Solanki <raj350@mail.fresnostate.edu>
2022-08-25 08:14:25 -0700

    updated script

--- scripts/laravel-refresh.sh
+++ scripts/laravel-refresh.sh
@@ -1,6 +1,7 @@
 cd ../backend
-composer dump-autoload --optimize
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
+php artisan config:clear
+php artisan cache:clear
+php artisan view:clear
+php artisan route:clear
+composer dump-autoload
 php artisan optimize
