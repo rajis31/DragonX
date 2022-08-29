9cfd01a6a7eddad6a2017be11a7abf8f16ca3ec8
Raj Solanki <raj350@mail.fresnostate.edu>
2022-08-29 12:45:35 -0700

    using npm package for app brige

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -14,12 +14,13 @@ export default {
     const apiKey       = '91e985092d5546317fcd5a4dd24f4243';
     const redirectUri  = 'https://dragonx.dev-top.com/register/" + "codeinspire-shop-2"';
     const urlParams    = new URLSearchParams(window.location.href);
-    let host           = urlParams.getItem("host") ? urlParams.getItem("host") :
+    let host           = urlParams.get("HOST") ? urlParams.get("HOST") :
                          this.$store.getters.getHost();
     this.$store.mutations.setHost(host);
 
     const permissionUrl = `https://${host}/admin/oauth/authorize?client_id=${apiKey}
-                       &scope=read_products,read_content&redirect_uri=${redirectUri}`;
+                           &scope=read_products,write_products,read_customers,write_customers,read_inventory,write_inventory
+                           &redirect_uri=${redirectUri}`;
 
     console.log(host);
 
